971a70e834b23d2437ec90fe2ecc5a4de7386b8f
yogesh07b208 <97577738+yogesh07b208@users.noreply.github.com>
2022-01-12 09:34:03 +0530

    AddedLogic

--- bowling-ball/BowlingBall/Game.cs
+++ bowling-ball/BowlingBall/Game.cs
@@ -8,15 +8,47 @@ namespace BowlingBall
 {
     public class Game
     {
-        public void Roll(int pins)
+        public void Roll(int firstRoll, int secondRoll)
         {
             // Add your logic here. Add classes as needed.
+            if (GameFinished())
+                throw new GameOverException();
+
+            Add(Frame.Create(firstRoll, secondRoll));
         }
 
         public int GetScore()
         {
             // Returns the final score of the game.
-            return 0;
+             Add(new Open(0, 0));
+            Add(new Open(0, 0));
+
+            for (int i = 0; i < 10; i++)
+                _frames[i].AddBonus(_frames[i + 1], _frames[i + 2]);
+
+            int _totalScore = 0;
+            _frames.ForEach(frame => _totalScore += frame.Score());
+            return _totalScore;
+            
+        }
+         public void RollStrike()
+        {
+            Roll(10, 0);
+        }
+
+        private bool GameFinished()
+        {
+            return _frames.Count.Equals(10);
+        }
+
+        private void Add(Frame frame)
+        {
+            _frames.Add(frame);
+        }
+
+        public void RollFinalFrame(int firstRoll, int secondRoll, int thirdRoll)
+        {
+            Add(Frame.Create(firstRoll, secondRoll, thirdRoll));
         }
     }
 }
